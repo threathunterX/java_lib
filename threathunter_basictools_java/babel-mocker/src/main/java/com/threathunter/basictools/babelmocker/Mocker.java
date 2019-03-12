32aa0812571d55fa27a54cfab8b21df4ddda5206
fanai <zengjinping@threathunter.cn>
2019-03-12 12:33:10 +0800

    rm err module

--- threathunter_basictools_java/babel-mocker/src/main/java/com/threathunter/basictools/babelmocker/Mocker.java
+++
@@ -1,8 +0,0 @@
-package com.threathunter.basictools.babelmocker;
-
-/**
- * Created by daisy on 17-11-8
- */
-public interface Mocker {
-
-}
--- threathunter_common_java/1.0.3/src/main/java/com/threathunter/common/God.java
+++
@@ -1,8 +0,0 @@
-package com.threathunter.common;
-
-/**
- * @author Wen Lu
- */
-public interface God {
-    public long tellTime();
-}
